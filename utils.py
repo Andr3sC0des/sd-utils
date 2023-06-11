582c7d10d6a23168fb82cd7dbb1b602c1c769e33
Andr3sC0des <andresqcodes@gmail.com>
2023-06-11 16:43:29 -0600

    Fixing errors

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 def clone(webui_version):
     return f'git clone -b {webui_version} https://github.com/Andr3sC0des/stable-diffusion-webui'
+
 def name():
     return 'stable-diffusion-webui'
+
 def controlNet(PATH):
     return f'git clone https://github.com/Mikubill/sd-webui-controlnet {PATH}/stable-diffusion-webui/extensions/sd-webui-controlnet'
