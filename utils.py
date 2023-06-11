4bc88116a1e0fc6caeff839e0408f0487c9a3dd2
Andr3sC0des <andresqcodes@gmail.com>
2023-06-11 16:38:22 -0600

    Adding controlnet

--- utils.py
+++ utils.py
@@ -2,3 +2,5 @@ def clone(webui_version):
     return f'git clone -b {webui_version} https://github.com/Andr3sC0des/stable-diffusion-webui'
 def name():
     return 'stable-diffusion-webui'
+def controlNet(PATH):
+    return f'git clone https://github.com/Mikubill/sd-webui-controlnet {PATH}/stable-diffusion-webui/extensions/sd-webui-controlnet'
