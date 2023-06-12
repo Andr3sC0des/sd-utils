edfe224db1be01236c26cd788f80e19b2bafb9a3
Andr3sC0des <andresqcodes@gmail.com>
2023-06-11 20:27:08 -0600

    Changing name

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def clone(webui_version):
     return f'git clone -b {webui_version} https://github.com/Andr3sC0des/stable-diffusion-webui'
 
 def name():
-    return 'stable-diffusion-webui'
+    return 'personal-gen'
 
 def controllink():
     return 'https://github.com/Mikubill/sd-webui-controlnet'
